346e84baa4ee8462ca4ff9b125578ce4815d8c44
zbennett10 <zbennett10@Hotmail.com>
2016-11-23 10:19:30 -0500

    Rendering basic React components

--- MarkdownPreviewerReact/Scripts/MarkdownPractice.jsx
+++ MarkdownPreviewerReact/Scripts/MarkdownPractice.jsx
@@ -1,53 +1,36 @@
 ﻿var CommentBox = React.createClass({
-    render: function () {
-        return (
-            <div className="commentBox">
-               <h1>Comments</h1>
-               <CommentList data="this.props.data" />
-               <CommentForm />
-            </div>);
-    }
-});
-
-var Comment = React.createClass({
-    render: function () {
-        return (
-            <div className="comment">
-                <h2 className="commentAuthor">
-                    {this.props.author}
-                </h2>
-                {this.props.children}
-            </div>
-            )
-    }
+  render: function() {
+    return (
+      <div className="commentBox">
+          <h1>Comments</h1>
+          <CommentList/>
+          <CommentForm/>
+      </div>
+    );
+  }
 });
 
 var CommentList = React.createClass({
-    render: function () {     
-        var commentNodes = this.props.data.map(function(comment) {
-            return (
-                <Comment author={comment.author} key={comment.id}>
-                    {comment.text}
-                </Comment>
-                );
-        
-         });
+    render: function() {
         return (
-            <div className="commentList">
-               {commentNodes}
-            </div>
-            );
-        }
-        });
+          <div className="commentList">
+            Hello, world! I am a CommentList.
+          </div>
+      );
+    }
+});
 
-//json - this would be inside the model
-var data = [
-  { id: 1, author: "Daniel Lo Nigro", text: "Hello ReactJS.NET World!" },
-  { id: 2, author: "Pete Hunt", text: "This is one comment" },
-  { id: 3, author: "Jordan Walke", text: "This is *another* comment" }
-];
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+          <div className="commentForm">
+            Hello, world! I am a CommentForm.
+          </div>
+      );
+    }
+});
 
 ReactDOM.render(
-    <CommentBox data={data} />,
-    document.getElementById('content')
-    );
+  <CommentBox />,
+  document.getElementById('content')
+);
